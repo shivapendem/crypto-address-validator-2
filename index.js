8e083469e965c31d0997903e31f2a4d307f3de0a
shivapendem <pendem.shiva89@gmail.com>
2024-11-09 18:50:31 +0530

    added diamante

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var WAValidator = require('multicoin-address-validator');
 
 function validate(_adddress, currencyid) {
+      if (isEmptyOrNull(currencyid) || isEmptyOrNull(_adddress)) {
+            return false;
+      }
       var currency = WAValidator.findCurrency(currencyid);
       if (currency)
             return WAValidator.validate(_adddress, currencyid);
@@ -8,6 +11,9 @@ function validate(_adddress, currencyid) {
             switch (currencyid.toString().toLowerCase().trim()) {
                   case 'near':
                         return validateImplicitAccountNear(_adddress) || validateNamedAccountNear(_adddress);
+                  case 'diamante':
+                  case 'diam':
+                        return validateNamedAccountDiamante(_adddress);
                   case 'xdc':
                   case 'xinfin':
                         return (_adddress.toLowerCase().indexOf("xdc") == 0) ? WAValidator.validate(_adddress.replace("xdc", "0x"), 'eth') : false;
@@ -28,6 +34,26 @@ function validateImplicitAccountNear(accountId) {
       const implicitAccountPattern = /^[0-9a-f]{64}$/;
       return implicitAccountPattern.test(accountId);
 }
-// console.log(validate("xdce50d5fc9bcbce037a19c860ba4105548d42517a0", "xdc"));
-// console.log(validate("0xe50d5fc9bcbce037a19c860ba4105548d42517a0", "xdc"));
+function validateNamedAccountDiamante(accountId) {
+      const accountPattern = /^G[A-Z2-7]{55}$/;
+      return accountPattern.test(accountId);
+}
+function isEmptyOrNull(data) {
+      return isEmpty(data) ? isEmpty(data) : isNull(data);
+};
+function isEmpty(string) {
+      if (isNull(string)) return true;
+      else if ((typeof string) == "object") { return Object.keys(string).length == 0 ? true : false; }
+      else if ((typeof string) == "boolean") return false;
+      else if ((typeof string) == "number") return false;
+      else if (Array.isArray(string)) { string.length == 0 ? true : false; }
+      else if (string.trim() === "") return true;
+      else return false;
+};
+function isNull(data) {
+      if ((data === null) || (typeof data === 'undefined')) {
+            return true;
+      }
+      return false;
+}
 module.exports = { validate };
